7bc99042382e451b1f9f13193e96694d4bc024e3
aleksvilly <49431859+aleksvilly@users.noreply.github.com>
2026-08-02 23:16:18 +0300

    migration to Astro

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://aleksvilly.github.io',
+  base: '/ai-booklet-designs',
+  output: 'static',
+  build: {
+    format: 'file'
+  }
+});
